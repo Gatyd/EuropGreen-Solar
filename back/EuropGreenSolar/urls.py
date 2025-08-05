7d459787d86d73bed92895eb8cc8d6d9f414675d
Gatyd <devgatyd@gmail.com>
2025-08-05 10:42:27 +0100

    Retouche gestion des utilisateurs

--- back/EuropGreenSolar/urls.py
+++ back/EuropGreenSolar/urls.py
@@ -15,8 +15,26 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    # API Documentation
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    
+    # Authentication URLs
+    # path('auth/', include('authentication.urls')),
+    
+    # Users URLs
+    path('', include('users.urls')),
 ]
+
+# Serving media files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
