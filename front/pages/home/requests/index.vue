b5ddd854194bf3940b00164c975c0d4e20f05663
Gatyd <devgatyd@gmail.com>
2025-08-15 22:47:09 +0100

    Initialisation module demandes backend

---
+++ front/pages/home/requests/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="sticky top-0 z-50 bg-white">
+        <UDashboardNavbar title="Demandes" class="lg:text-2xl font-semibold"
+            :ui="{ root: 'h-12 lg:h-(--ui-header-height)' }"><template #trailing>
+                <UBadge variant="subtle" />
+            </template>
+            <template #right>
+                <UButton color="primary" label="Nouvelle demande" icon="i-heroicons-plus" class="mx-2"
+                    @click="" />
+            </template>
+        </UDashboardNavbar>
+    </div>
+</template>
